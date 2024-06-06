fdbea2402d276b002b61363fd5b92ec55dd647db
Maslinisto <romlitvinov2002@gmail.com>
2024-06-06 21:11:21 +0300

    fixed sessionmaker

--- app/database.py
+++ app/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import DeclarativeBase, sessionmaker
 
 engine = create_engine("postgresql+psycopg2://postgres:bebra@localhost:5432/marketplace")
-sessionmaker = sessionmaker(bind=engine)
-session = sessionmaker()
+session = sessionmaker(bind=engine)
+#sessionmaker = sessionmaker()
 class Base(DeclarativeBase):
     pass
